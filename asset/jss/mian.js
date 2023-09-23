ebf2b4a372701acbf59849549925bacd6e260d6c
ashraful-rahat <spinball61@gamil.com>
2023-09-24 00:02:50 +0600

    first

---
+++ asset/jss/mian.js
@@ -0,0 +1,13 @@
+import Swiper from 'swiper';
+import 'swiper/css'
+
+
+
+
+
+var swiperCategories = new Swiper('categories__container', {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
